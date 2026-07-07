22769fc4a2ed72b97af1a6badbd73591f12a4ddb
yyubin <hazing120@gmail.com>
2026-07-07 15:06:08 +0900

    feat: wrap account context plugin with fastify-plugin for proper request handling

--- bff/src/plugins/account-context.plugin.ts
+++ bff/src/plugins/account-context.plugin.ts
@@ -1,4 +1,5 @@
 import type { FastifyPluginAsync } from 'fastify';
+import fp from 'fastify-plugin';
 import { verifyToken } from './jwt';
 
 function bearerToken(value: unknown): string | null {
@@ -21,4 +22,7 @@ const accountContextPlugin: FastifyPluginAsync = async (app) => {
   });
 };
 
-export default accountContextPlugin;
+// fastify-plugin으로 감싸 캡슐화를 해제한다. 감싸지 않으면 onRequest 훅이
+// 이 플러그인 컨텍스트에만 적용돼 이후 register된 라우트에는 x-account-id가
+// 세팅되지 않아 전부 401이 된다. (grpc-registry / pubsub 와 동일 패턴)
+export default fp(accountContextPlugin, { name: 'account-context' });
